0be329273471e9d9a719617cd93890dd3c3efd0c
lpxxn <mi_duo@live.com>
2019-05-27 13:29:42 +0800

    gomicor default register

--- example1/cli/main.go
+++ example1/cli/main.go
@@ -10,8 +10,8 @@ import (
 )
 
 func main() {
-	// 我这里用的etcd 做为服务发现，如果使用consul可以去掉
-	reg := etcdv3.NewRegistry(func(op *registry.Options){
+	// 我这里用的etcd 做为服务发现
+	reg := etcdv3.NewRegistry(func(op *registry.Options) {
 		op.Addrs = []string{
 			"http://192.168.3.34:2379", "http://192.168.3.18:2379", "http://192.168.3.110:2379",
 		}
@@ -22,17 +22,17 @@ func main() {
 		micro.Registry(reg),
 	)
 
-	// 如果你用的是consul把上面的注释掉用下面的
+	// 2019年源码有变动默认使用的是mdns面不是consul了
+	// 如果你用的是默认的注册方式把上面的注释掉用下面的
 	/*
-	// 初始化服务
-	service := micro.NewService(
-		micro.Name("lp.srv.eg1"),
-	)
-	 */
+		// 初始化服务
+		service := micro.NewService(
+			micro.Name("lp.srv.eg1"),
+		)
+	*/
 	service.Init()
 
 	sayClent := model.NewSayService("lp.srv.eg1", service.Client())
-
 
 	rsp, err := sayClent.Hello(context.Background(), &model.SayParam{Msg: "hello server"})
 	if err != nil {
--- example1/srv/main.go
+++ example1/srv/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/micro/go-plugins/registry/etcdv3"
 )
 
-type Say struct {}
+type Say struct{}
 
 func (s *Say) Hello(ctx context.Context, req *model.SayParam, rsp *model.SayResponse) error {
 	fmt.Println("received", req.Msg)
@@ -23,10 +23,9 @@ func (s *Say) Hello(ctx context.Context, req *model.SayParam, rsp *model.SayResp
 	return nil
 }
 
-
 func main() {
-	// 我这里用的etcd 做为服务发现，如果使用consul可以去掉
-	reg := etcdv3.NewRegistry(func(op *registry.Options){
+	// 我这里用的etcd 做为服务发现
+	reg := etcdv3.NewRegistry(func(op *registry.Options) {
 		op.Addrs = []string{
 			"http://192.168.3.34:2379", "http://192.168.3.18:2379", "http://192.168.3.110:2379",
 		}
@@ -38,13 +37,14 @@ func main() {
 		micro.Registry(reg),
 	)
 
-	// 如果你用的是consul把上面的注释掉用下面的
+	// 2019年源码有变动默认使用的是mdns面不是consul了
+	// 如果你用的是默认的注册方式把上面的注释掉用下面的
 	/*
-	// 初始化服务
-	service := micro.NewService(
-		micro.Name("lp.srv.eg1"),
-	)
-	 */
+		// 初始化服务
+		service := micro.NewService(
+			micro.Name("lp.srv.eg1"),
+		)
+	*/
 	service.Init()
 
 	// 注册 Handler
